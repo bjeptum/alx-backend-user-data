e0cf748dae1b9c8bea15c4028703afca942d09ae
b_jeptum <brendajeptum@gmail.com>
2024-11-13 01:30:13 +0300

    Unauthorized access handling updated

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -27,7 +27,7 @@ def stats() -> str:
 
 
 @app_views.route('/unauthorized/', methods=['GET'], strict_slashes=False)
-def unauthorized() -> str:
+def unauthorized(error) -> str:
     """ GET /api/v1/unauthorized
     Returns:
     str: _description_
