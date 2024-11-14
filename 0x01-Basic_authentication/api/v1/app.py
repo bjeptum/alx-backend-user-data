b551a14db7a33a0d52cc19284f04a7165be0cbb8
b_jeptum <brendajeptum@gmail.com>
2024-11-14 12:19:49 +0300

    Validate all requests to secure API

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -8,11 +8,17 @@ from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
 from typing import Tuple
+from api.v1.auth.auth import Auth
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+auth = None
+auth_type = getenv('AUTH_TYPE')
+
+if auth_type == "Auth":
+    auth = Auth()
 
 
 @app.errorhandler(404)
@@ -34,6 +40,21 @@ def forbidden(error) -> Tuple[str, int]:
     """ Forbidden handler
     """
     return jsonify({"error": "Forbidden"}), 403
+
+
+@app.before_request
+def before_request():
+    """Handler to filter requests before reaching route"""
+    if auth is None:
+        return None
+    exempt = ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']
+    if auth.require_auth(request.path, exempt):
+        # Check for auth header
+        if auth.authorization_header(request) is None:
+            abort(401)
+        # Check current user
+        if auth.current_user(request) is None:
+            abort(403)
 
 
 if __name__ == "__main__":
