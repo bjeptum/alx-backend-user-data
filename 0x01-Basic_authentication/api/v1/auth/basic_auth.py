1110acd686d7fd0bad560c9c630b0a102a4dd7cb
b_jeptum <brendajeptum@gmail.com>
2024-11-15 17:44:17 +0300

    Basic - Base64 part

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,8 +3,27 @@
 BasicAuth module.
 """
 from api.v1.auth.auth import Auth
+from flask import Flask, request, Response
+import base64
 
 
 class BasicAuth(Auth):
     """BasicAuth class that inherits from Auth."""
-    pass
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """Returns base64 encoded"""
+        if authorization_header is None:
+            return None
+        if not isinstance(authorization_header, str):
+            return None
+        if not authorization_header.startswith("Basic "):
+            return None
+        # Extract the part after "Basic"
+        cred_header = authorization_header[len("Basic "):]
+        return cred_header.strip()
+
+        """
+        Encode the extracted part to Base64
+        base64_encoded = base64.b64encode(cred_header.encode('utf-8'))
+        return base64_encoded.decode('utf-8')
+        """
