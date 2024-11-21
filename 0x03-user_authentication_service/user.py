07c406c3cba45eeec3eefdf92773100f2f7cc825
b_jeptum <brendajeptum@gmail.com>
2024-11-21 23:18:43 +0300

    Find user method added

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -4,6 +4,8 @@ import sqlalchemy
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
 
 
 Base = declarative_base()
